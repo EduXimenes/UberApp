a68c8b46f42c9f385b376c50dd527f3bbe9b7eb7
EduXimenes <e.ximenes17@hotmail.com>
2022-10-09 12:13:08 -0300

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Eduardo Ximenes on 09/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
