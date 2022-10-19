917b66187f6fb9fd6cf92db162acbcb3cb67df1c
EduXimenes <e.ximenes17@hotmail.com>
2022-10-19 10:32:12 -0300

    First Commit with the user's location

---
+++ Uber/Core/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Uber
+//
+//  Created by Eduardo Ximenes on 09/10/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+            .ignoresSafeArea()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
